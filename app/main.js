e14edd5494ecaf421ba8487db08e93e2d8c9a0cf
jiangjianqing <cz_jjq@163.com>
2015-12-01 15:31:41 +0800

    add main.js and index.html

--- app/main.js
+++ app/main.js
@@ -1,3 +1,6 @@
+/**
+ * Created by ztxs on 15-9-1.
+ */
 /*global require*/
 'use strict';
 
@@ -6,6 +9,12 @@ require.config({
 	// The shim config allows us to configure dependencies for
 	// scripts that do not call define() to register a module
 	shim: {
+		bootstrap:{
+			deps: [
+				'jquery'
+			],
+			exports:'bootstrap'
+		},
 		underscore: {
 			exports: '_'
 		},
@@ -13,55 +22,41 @@ require.config({
 			deps: [
 				'underscore',
 				'jquery'
-			],
-			exports: 'Backbone'
-		},
-		backboneLocalstorage: {
-			deps: ['backbone'],
-			exports: 'Store'
-		},
-		jqueryuiLayout:{
-			deps:[
-				'jqueryui'
 			]
-		},
-		jqgrid:{
-			deps:['jqgridlocale']
-		},
-		jqgridlocale:{
-			deps:['jqueryui']
 		}
 	},
 	paths: {
-		jquery: '../node_modules/jquery/jquery',
-		jqueryui:'../node_modules/jquery/jquery-ui',
-		jqueryuiLayout:'../node_modules/jquery/jquery.layout-latest',
-		jqueryThemeHelper:'../node_modules/jquery/jquery.themehelper',
-		jqgridlocale: 'https://cdn.bootcss.com/jqgrid/4.6.0/js/i18n/grid.locale-cn',
-		jqgrid: 'https://cdn.bootcss.com/jqgrid/4.6.0/js/jquery.jqGrid.min',
-		jstree:'../node_modules/jquery/jstree',
-		mockjax:'../node_modules/jquery/jquery.mockjax',
-		underscore: '../node_modules/underscore/underscore',
-		backbone: '../node_modules/backbone/backbone',
-		backboneLocalstorage: '../node_modules/backbone.localstorage/backbone.localStorage',
-		text: '../node_modules/requirejs/text',
-		domReady:'../node_modules/requirejs/domReady',
-		handlebars:'../node_modules/handlebars/handlebars'
+		bootstrap:'../bower_components/bootstrap/dist/js/bootstrap',
+		jquery: '../bower_components/jquery/dist/jquery',
+		underscore: '../bower_components/underscore/underscore',
+		backbone: '../bower_components/backbone/backbone',
+		text: '../bower_components/requirejs-text/text',
+		domReady:'../bower_components/requirejs-domready/domReady',
+		handlebars:'../bower_components/handlebars/handlebars'
 	}
 });
 
 require([
 	'backbone',
-	'views/app',
-	'routers/router'
-], function (Backbone, AppView, Workspace) {
-	/*jshint nonew:false*/
-	// Initialize routing and start Backbone.history()
-	console.log("main被调用");
-	new Workspace();
+	'jquery',
+	//'views/group/groups',
+	//'routers/router',
+	'domReady'
+], function (backbone,$/*GroupView,AppRouter*/) {
 
-	Backbone.history.start();
+	//一个应用中只有一个router，所有的路由都有其管理，这里将其管理的el范围传递进去
+	//var router = new AppRouter('#appView');//20150909,Router功能在Activiti-Explorer中使用
+	//普通Backbone测试无需使用AppRouter
 
-	// Initialize the application view
-	new AppView();
+	//20150906,使用Html5的pushState特性，服务器端也必须设定Rewrite attributes，尚未测试
+	backbone.history.start(/*{pushState : true,root: '/'}*/);
+	/**启用 HTML5 特性 pushState 的配置调用 start() 方法。
+	 * 对于那些支持 pushState 的浏览器，Backbone 将监视 popstate 事件以触发一个新状态。
+	 * 如果浏览器不能支持 HTML5 特性，那么 onhashchange 活动会被监视。
+	 * 如果浏览器不支持该事件，轮询技术将监视 URL 散列片段的任何更改*/
+
+	console.log(" main is run!!");
+	//var groupsView=new GroupView();//临时测试用，正式项目中应该交由AppRouter处理
+	//$("#appView").html(groupsView.render().$el);//让视图通用的写法
+	//$("head>title").text("Group管理");
 });
